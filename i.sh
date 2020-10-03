7c41c5a83362104ad75e31312a89da87cd478876
Muhamad Fathoni <50394981+muhamadfathoni27rpl@users.noreply.github.com>
2020-10-03 20:08:26 +0700

    Add files via upload

---
+++ i.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt-get install snort
+apt-get install bison libpcap-dev make flex luajit libdumbnet-dev
+apt-get install libtool libtool-bin libpcre3 libpcre3-dev
+apt-get install libdumbnet-dev libssl-dev libdaq-dev
+apt-get install zlib1g-dev liblua5.1-0-dev liblua5.2-dev libluajit-5.1-dev
+wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz
+wget https://www.snort.org/downloads/snort/snort-2.9.16.1.tar.gz
+#etc.
